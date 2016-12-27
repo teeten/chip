c1a3a71b8dcc765956039e32c3f952b0cd8be1e3
teeten <jc.dhellemmes@free.fr>
2016-12-27 23:38:25 +0000

    use library in autopower

--- python/autopoweroff.py
+++ python/autopoweroff.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 
 import time, signal, sys
-import CHIP_IO.GPIO as GPIO
-from axp209 import AXP209
+import Adafruit_PureIO.smbus as smbus
+import t10_axp209 as pmu
 
 def cleanup(sig, ctx):
-    GPIO.output("XIO-P1", GPIO.HIGH)
     print "\nbye"
-    axp.close()
-    GPIO.cleanup()
+    axp209.close()
     sys.exit(0)
 
 signal.signal(signal.SIGINT, cleanup)
@@ -16,33 +14,25 @@ signal.signal(signal.SIGINT, cleanup)
 MAXCOUNT  = 20
 countdown = MAXCOUNT
 period    = 5
+axp209    = smbus.SMBus(0, dangerous=False)
 ts0       = time.time()
 
-GPIO.setup("XIO-P1", GPIO.OUT)
-GPIO.output("XIO-P1", GPIO.HIGH)
-
 while 1:
-    time.sleep(period)
-
-    axp = AXP209()
     ts = time.time() - ts0
-    
-    print "TIME=%f S=%d ACIN=%f VBUS=%f BATT=%f DCHG=%f COUNT=%d" % (ts, axp.power_input_status.acin_available, 0, 0, 0, 0, countdown)
+ 
+    power_out = not pmu.ison(pmu.power_status(axp209), pmu.HAS_ACIN)
+    print("TIME=%04d POWER_OUT=%s COUNT=%d" % (ts, power_out, countdown))
 
     if (countdown == 0):
+        axp209.close()
         print "shutdown"
-        cleanup()
         sys.exit(0)
 
-    if (axp.power_input_status.acin_available == 0): # no power
+    if (power_out):
         period = 1
         countdown -= 1
-        GPIO.output("XIO-P1", GPIO.HIGH)
-        time.sleep(0.1)
-        GPIO.output("XIO-P1", GPIO.LOW)
-
     else:
-        GPIO.output("XIO-P1", GPIO.HIGH)
         period = 5
         countdown = MAXCOUNT
 
+    time.sleep(period)

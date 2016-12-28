9ba05c06b0324082488faf6feabb5e88e922f53c
teeten <jc.dhellemmes@free.fr>
2016-12-28 21:46:47 +0000

    use syslog

--- python/autopoweroff.py
+++ python/autopoweroff.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python
 
-import time, signal, sys
+import time, signal, sys, syslog, os
 import Adafruit_PureIO.smbus as smbus
 import t10_axp209 as pmu
 
-def cleanup(sig, ctx):
-    print "\nbye"
+def bye(sig, ctx):
+    syslog.syslog("signal %d caught, terminating ..." % sig)
     axp209.close()
     sys.exit(0)
 
-signal.signal(signal.SIGINT, cleanup)
+syslog.openlog("autopoweroffd", syslog.LOG_PID, syslog.LOG_DAEMON)
+syslog.syslog("init")
+
+signal.signal(signal.SIGINT, bye)
+signal.signal(signal.SIGTERM, bye)
 
 MAXCOUNT  = 20
 countdown = MAXCOUNT
 period    = 5
-axp209    = smbus.SMBus(0, dangerous=False)
-ts0       = time.time()
+axp209    = smbus.SMBus(0)
+
+syslog.syslog("start")
 
 while 1:
-    ts = time.time() - ts0
- 
     power_out = not pmu.ison(pmu.power_status(axp209), pmu.HAS_ACIN)
-    print("TIME=%04d POWER_OUT=%s COUNT=%d" % (ts, power_out, countdown))
-
     if (countdown == 0):
+        syslog.syslog("timeout reached, shutting down ...")
         axp209.close()
-        print "shutdown"
-        sys.exit(0)
-
+        os.system("init 0")
     if (power_out):
-        period = 1
+        if (countdown == MAXCOUNT):
+            syslog.syslog("power out detected")
+            period = 1
         countdown -= 1
     else:
-        period = 5
-        countdown = MAXCOUNT
-
+        if (countdown != MAXCOUNT):
+            syslog.syslog("power is back on (%ds from timeout)" % countdown)
+            period = 5
+            countdown = MAXCOUNT
     time.sleep(period)
